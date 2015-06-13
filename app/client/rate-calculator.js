c6e41ca3ac45733840c23ec176fb466e93751898
xcv58 <chenyihonglove@gmail.com>
2015-06-13 09:57:27 +0800

    init.

---
+++ app/client/rate-calculator.js
@@ -0,0 +1,23 @@
+Template.rate.helpers({
+  startDate: function() {
+    return 1;
+  },
+  endDate: function() {
+    return 2;
+  },
+  dayLength: function() {
+    return 3;
+  },
+  base: function() {
+    return '本金';
+  },
+  rate: function() {
+    return '利率';
+  },
+  interest: function() {
+    return '利息';
+  },
+  total: function() {
+    return '总金额';
+  }
+});
